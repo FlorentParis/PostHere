ecdf8853127456ca98ae212f3931da12fa49dbd6
Giselle MATIAS <giselle.magalong@gmail.com>
2021-12-06 12:17:05 +0100

    Add vendor

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6fc14e1693e336a6132e8fca54f8ab3071902d8b',
+    'name' => 'flogigi/posthere',
+  ),
+  'versions' => 
+  array (
+    'flogigi/posthere' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6fc14e1693e336a6132e8fca54f8ab3071902d8b',
+    ),
+    'michelf/php-markdown' => 
+    array (
+      'pretty_version' => '1.9.1',
+      'version' => '1.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5024d623c1a057dcd2d076d25b7d270a1d0d55f3',
+    ),
+  ),
+);
