823dc0f5250512f6eb6851ae003237958bb28c46
Giselle MATIAS <giselle.magalong@gmail.com>
2021-12-08 21:33:59 +0100

    Update inscription

--- app/src/models/ConnexionManager.php
+++ app/src/models/ConnexionManager.php
@@ -11,8 +11,13 @@ class ConnexionManager extends BaseManager
         return $search->fetch();
     }
 
-    /*
-    public function registerUser(){
-        $insert = $pdo->prepare('INSERT INTO user VALUES('')')
-    }*/
+    public function addUser(array $data)
+    {
+        //var_dump($data);
+        //var_dump($data);
+        $sql = "INSERT INTO users(name, first_name, email, password, admin) VALUES ('".$data[0]."', '".$data[1]."', '".$data[2]."', '".$data[3]."','".$data[4]."')";
+        $result = $this->pdo->prepare($sql);
+        $result->execute($data);
+        //var_dump($result);
+    }
 }
