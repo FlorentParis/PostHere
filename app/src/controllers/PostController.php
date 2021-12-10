caf08f197cfbb843a04fc1146dfbba957a1ea8ee
Florent <florentparis28@gmail.com>
2021-12-10 15:09:10 +0100

    beging image

--- app/src/controllers/PostController.php
+++ app/src/controllers/PostController.php
@@ -32,6 +32,14 @@ class PostController extends BaseController
 
     public function executeSendCreatePost()
     {
+        if(isset($_FILES['file'])){
+            $tmpName = $_FILES['file']['tmp_name'];
+            $name = $_FILES['file']['name'];
+            $size = $_FILES['file']['size'];
+            $error = $_FILES['file']['error'];
+
+            move_uploaded_file($tmpName, './upload/'.$name);
+        }
         if ( $_POST['title'] != null && $_POST['content'] != null ){
             $post = new Post();
             $post->setTitle($_POST['title']);
--- app/src/views/Frontend/createPost.php
+++ app/src/views/Frontend/createPost.php
@@ -1,7 +1,11 @@
-<form action="sendcreatepost" method="post" class="p-4 m-4 border">
+<form action="sendcreatepost" method="post" enctype="multipart/form-data" class="p-4 m-4 border">
     <div class="d-flex flex-column">
         <label for="title">Title</label>
         <input type="text" name="title" class="w-25">
+    </div>
+    <div class="d-flex flex-column">
+        <label for="picture">Image</label>
+        <input type="file" name="picture">
     </div>
     <div>
         <label for="content">Contenu</label>
